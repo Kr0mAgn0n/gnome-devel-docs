b4986544b2ead0109927aca40635da7671aec6e0
P. F. Chimento <philip.chimento@gmail.com>
2011-03-22 18:42:42 +0100

    Ran through Guitar Tuner in Python

--- platform-demos/C/guitar-tuner/guitar-tuner.py
+++ platform-demos/C/guitar-tuner/guitar-tuner.py
@@ -5,18 +5,20 @@ import os, sys
 
 #Comment the first line and uncomment the second before installing
 #or making the tarball (alternatively, use project variables)
-UI_FILE = "guitar_tuner_py.ui"
+UI_FILE = "src/guitar_tuner_py.ui"
 #UI_FILE = "/usr/local/share/guitar_tuner_py/ui/guitar_tuner_py.ui"
 
 class GUI:
 	LENGTH = 500
 	# Frequencies of the strings
-	NOTE_E = 369.23
-	NOTE_A = 440
-	NOTE_D = 587.33
-	NOTE_G = 783.99
-	NOTE_B = 987.77
-	NOTE_e = 1318.5
+	frequencies = {
+		'E': 369.23,
+		'A': 440,
+		'D': 587.33,
+		'G': 783.99,
+		'B': 987.77,
+		'e': 1318.5
+	}
 	
 	def __init__(self):
 		self.builder = Gtk.Builder()
@@ -26,26 +28,17 @@ class GUI:
 		window = self.builder.get_object('window')
 		window.show_all()
 
-
 	def on_button_clicked (self, button):
 		label = button.get_child()
 		text = label.get_label()
-		
-		if text == "E":
-			self.play_sound (self.NOTE_E)
-		elif text == "A":
-			self.play_sound (self.NOTE_A)
-		elif text == "G":
-			self.play_sound (self.NOTE_G)
-		elif text == "D":
-			self.play_sound (self.NOTE_D)
-		elif text == "B":
-			self.play_sound (self.NOTE_B)
-		elif text == "e":
-			self.play_sound (self.NOTE_e)
-			
+		self.play_sound (self.frequencies[text])
+
 	def destroy(window, self):
 		Gtk.main_quit()
+
+	def pipeline_stop(self, pipeline):
+		pipeline.set_state(Gst.State.PAUSED)
+		return False
 
 	def play_sound(self, frequency):
 		pipeline = Gst.Pipeline(name='note')
@@ -61,13 +54,6 @@ class GUI:
 		pipeline.set_state(Gst.State.PLAYING)
 
 		GObject.timeout_add(self.LENGTH, self.pipeline_stop, pipeline)
-
-	def pipeline_stop(self, pipeline):
-		pipeline.set_state(Gst.State.PAUSED)
-		return False
-
-	
-
 		
 def main():
 	Gst.init_check(sys.argv)
