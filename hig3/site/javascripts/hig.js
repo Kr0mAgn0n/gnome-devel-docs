24a0a77193d5fea5940bb7b0882d9f84585ffbe1
Jakub Steiner <jimmac@gmail.com>
2012-01-19 21:08:11 +0100

    collapsing menu for the sidebar.

--- hig3/site/javascripts/hig.js
+++ hig3/site/javascripts/hig.js
@@ -1,5 +1,5 @@
 function positionFooter() {
-  var $footer = $('#footer');
+  var $footer = $('#footerpositioner');
   $footer.removeClass('fixedfooter');
   if ($(document.body).height() < $(window).height()) {
     $footer.addClass('fixedfooter');
@@ -8,9 +8,33 @@ function positionFooter() {
   }
 }
 
+$.fn.expander = function () {
+  $(this).each(function () {
+    $(this).click(function () {
+      if ($(this).hasClass('expand')) {
+        //hide
+        $(this).removeClass('expand').addClass('collapse');
+        $(this).siblings('ul').hide(200, function () {
+          $(window).resize(positionFooter).resize();
+        });
+      } else {
+        //show
+        $(this).removeClass('collapse').addClass('expand');
+        $(this).siblings('ul').show(200, function () {
+          $(window).resize(positionFooter).resize();
+        });
+      }
+    }).click();
+  });
+}
+
+
 $(document).ready(function () {
+  var $items = $(".sidebar .collapsed>li>a");
+
+  $items.expander();  
   $(window).resize(positionFooter).resize();
-  console.log('yay');
+
   //preventFOUT();
 });
 $(window).ready(function () {
