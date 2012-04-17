3c60048d6307bf3ef796f81398a733d328f8d6ac
Tiffany Antopolski <tiffany.antopolski@gmail.com>
2012-04-17 12:13:11 -0400

    JS samples: repurposed button sample into a Gtk.Grid sample.
    Also, updated image to be Gnome3 theme (was Ubuntu).
    Updated makefile with these changes.

---
+++ platform-demos/C/samples/grid.js
@@ -0,0 +1,18 @@
+#!/usr/bin/gjs
+Gtk = imports.gi.Gtk;
+Gtk.init(null, 0);
+myW = new Gtk.Window({type: Gtk.WindowType.TOPLEVEL});
+myW.title = "A Gtk.Grid";
+myW.connect("destroy", function(){Gtk.main_quit()});
+grid = new Gtk.Grid();
+myW.add(grid);
+this.pbar = new Gtk.ProgressBar();
+function foo(){
+  this.pbar.pulse();
+}
+var button = new Gtk.Button({label: "Button"});
+button.connect("clicked", foo);
+grid.attach(button, 1, 1, 1, 1);
+grid.attach_next_to(pbar, button, 3,1,1);
+myW.show_all();
+Gtk.main();
