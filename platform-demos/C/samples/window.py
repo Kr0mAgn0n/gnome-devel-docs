8add177bc09141401b06b1c232ed94108ddd648c
Tiffany Antopolski <tiffany.antopolski@gmail.com>
2012-04-23 00:32:50 -0400

    python sample: GtkWindow using GtkApplication

---
+++ platform-demos/C/samples/window.py
@@ -0,0 +1,10 @@
+from gi.repository import Gtk
+import sys
+
+class MyApp(Gtk.Application):
+    def do_activate(self):
+        window = Gtk.Window(application = self)
+	window.set_title("Welcome to GNOME")
+        window.show_all()
+
+MyApp(application_id = "org.example.whatever").run(sys.argv)
