00931cc6a5de98ecae353ff5576049f7dcf0bc37
Marta Maria Casetti <mmcasetti@gmail.com>
2012-04-28 16:27:23 +0100

    tutorial python: window example with corrected indentation

--- platform-demos/C/samples/window.py
+++ platform-demos/C/samples/window.py
@@ -4,7 +4,7 @@ import sys
 class MyApp(Gtk.Application):
     def do_activate(self):
         window = Gtk.Window(application = self)
-	window.set_title("Welcome to GNOME")
+	    window.set_title("Welcome to GNOME")
         window.show_all()
 
 MyApp(application_id = "org.example.whatever").run(sys.argv)
