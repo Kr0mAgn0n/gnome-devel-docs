cd7d688ed2e4df5ec6c66c19d7ec05e681a5c0f5
Tiffany Antopolski <tiffany.antopolski@gmail.com>
2012-05-24 20:36:56 -0400

    Updated image.c to GtkApplicationWindow.  Also, small style change.

--- platform-demos/C/samples/image.c
+++ platform-demos/C/samples/image.c
@@ -8,15 +8,15 @@ activate (GtkApplication *app,
   GtkWidget *window;
   GtkWidget *image;
 
-  window = gtk_window_new (GTK_WINDOW_TOPLEVEL);
+  window = gtk_application_window_new (app);
 
   gtk_window_set_application (GTK_WINDOW (window), GTK_APPLICATION (app));
   gtk_window_set_title (GTK_WINDOW (window), "Welcome to GNOME");
   gtk_window_set_default_size (GTK_WINDOW (window), 300, 300);//may have to change size
   
-  image = gtk_image_new_from_file("gnome-image.png");
+  image = gtk_image_new_from_file ("gnome-image.png");
 
-  gtk_container_add(GTK_CONTAINER(window), GTK_WIDGET(image));
+  gtk_container_add (GTK_CONTAINER (window), GTK_WIDGET (image));
 
   gtk_widget_show_all (GTK_WIDGET (window));
 }
