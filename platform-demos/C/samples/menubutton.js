890a4584bfe0d1390e145df4ff58ad51b3d95811
Tiffany Antopolski <tiffany.antopolski@gmail.com>
2012-08-01 05:05:23 -0400

    Remove gettext from menubutton.js and some spacing adjustments.

--- platform-demos/C/samples/menubutton.js
+++ platform-demos/C/samples/menubutton.js
@@ -1,6 +1,5 @@
 #!/usr/bin/gjs
 
-const _ = imports.gettext.gettext;
 const Gio = imports.gi.Gio;
 const GLib = imports.gi.GLib;
 const Gtk = imports.gi.Gtk;
@@ -10,11 +9,9 @@ const Application = new Lang.Class ({
     Name: 'Application',
 
     //create the application
-    _init: function () {
-        this.application = new Gtk.Application ({
-            application_id: 'org.example.myapp',
-            flags: Gio.ApplicationFlags.FLAGS_NONE
-        });
+    _init: function() {
+        this.application = new Gtk.Application ({ application_id: 'org.example.myapp',
+                                                  flags: Gio.ApplicationFlags.FLAGS_NONE });
 
        //connect to 'activate' and 'startup' signals to the callback functions
        this.application.connect('activate', Lang.bind(this, this._onActivate));
@@ -23,78 +20,72 @@ const Application = new Lang.Class ({
 
     //create the UI (in this case it's just the ApplicationWindow)
     _buildUI: function() {
-        this._window = new Gtk.ApplicationWindow  ({ application: this.application,
+        this._window = new Gtk.ApplicationWindow({ application: this.application,
                                                    window_position: Gtk.WindowPosition.CENTER,
                                                    title: "Welcome to GNOME" });
-        this._window.set_default_size (600, 400);
-        this.grid = new Gtk.Grid ();
-        this._window.add (this.grid);
-        
-        
+        this._window.set_default_size(600, 400);
+        this.grid = new Gtk.Grid();
+        this._window.add(this.grid);
+
+
         this._menuButton = new Gtk.MenuButton();
-        this.grid.attach (this._menuButton, 0, 0, 1, 1 );
-        this.menu = new Gtk.Menu.new_from_model (this.menuModel);
-        
-        this.menu.show ();
-        this._menuButton.set_menu (this.menu );
-        this._menuButton.set_size_request (80, 35);
-        this._menuButton.show ();
-        
+        this.grid.attach(this._menuButton, 0, 0, 1, 1 );
+        this.menu = new Gtk.Menu.new_from_model(this.menuModel);
+
+        this.menu.show();
+        this._menuButton.set_menu(this.menu);
+        this._menuButton.set_size_request(80, 35);
+        this._menuButton.show();
+
         this._window.show_all();
     },
 
     _showNew: function() {
-    print ("New Menu. It doesn't do anything. It is only a demonstration.");
+    print("New Menu. It doesn't do anything. It is only a demonstration.");
     },
 
     _showAbout: function() {
-        print ("No AboutDialog here.  This is only a demonstration.");
+        print("No AboutDialog here.  This is only a demonstration.");
     },
-    
-
 
     //create the menu items and connect the signals to the callback functions.
     _initMenus: function() {
-   
-        let newAction = new Gio.SimpleAction ({ name: 'new' });
+        let newAction = new Gio.SimpleAction({ name: 'new' });
         newAction.connect('activate', Lang.bind(this,
             function() {
                 this._showNew();
             }));
         this.application.add_action(newAction);
 
-        let aboutAction = new Gio.SimpleAction ({ name: 'about' });
+        let aboutAction = new Gio.SimpleAction({ name: 'about' });
         aboutAction.connect('activate', Lang.bind(this,
             function() {
                 this._showAbout();
             }));
         this.application.add_action(aboutAction);
 
-        let quitAction = new Gio.SimpleAction ({ name: 'quit' });
+        let quitAction = new Gio.SimpleAction({ name: 'quit' });
         quitAction.connect('activate', Lang.bind(this,
             function() {
                 this._window.destroy();
             }));
-         this.application.add_action( quitAction );
-         
-        this.menuModel = new Gio.Menu();  
-        
-        this.menuItemNew = Gio.MenuItem.new( _("New"), 'app.new' );
-        this.menuItemAbout = Gio.MenuItem.new( _("About"), 'app.about' );
-        this.fileMenuItem = Gio.MenuItem.new( _("Other"), null);
-        
-        this.menuModel.append_item( this.menuItemNew );
-        this.menuModel.append_item( this.menuItemAbout );
-        
+         this.application.add_action(quitAction);
+
+        this.menuModel = new Gio.Menu();
+
+        this.menuItemNew = Gio.MenuItem.new("New", 'app.new');
+        this.menuItemAbout = Gio.MenuItem.new("About", 'app.about');
+        this.fileMenuItem = Gio.MenuItem.new("Other", null);
+
+        this.menuModel.append_item(this.menuItemNew);
+        this.menuModel.append_item(this.menuItemAbout);
+
         //submenu
         this.subMenu = new Gio.Menu();
         this.fileMenuItem.set_submenu(this.subMenu);
-        this.menuItemQuit = Gio.MenuItem.new( ("Quit"), 'app.quit' );
-        this.subMenu.append_item( this.menuItemQuit );
+        this.menuItemQuit = Gio.MenuItem.new("Quit", 'app.quit');
+        this.subMenu.append_item(this.menuItemQuit);
         this.menuModel.append_item(this.fileMenuItem);
-        
-        
-        
     },
 
     //callback function for 'activate' signal
@@ -111,5 +102,5 @@ const Application = new Lang.Class ({
 });
 
 //run the application
-let app = new Application ();
-app.application.run (ARGV);
+let app = new Application();
+app.application.run(ARGV);
