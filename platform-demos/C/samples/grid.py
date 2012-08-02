74bd36df7b44993a6f78173c19023dd4ce23a9c6
Marta Maria Casetti <mmcasetti@gmail.com>
2012-08-02 02:47:51 +0100

    tutorials python: grid widget rewritten

--- platform-demos/C/samples/grid.py
+++ platform-demos/C/samples/grid.py
@@ -1,31 +1,40 @@
 from gi.repository import Gtk
 import sys
 
-class GridWindow(Gtk.ApplicationWindow):
+class MyWindow(Gtk.ApplicationWindow):
     def __init__(self, app):
         Gtk.Window.__init__(self, title="Grid Example", application=app)
 
+        # three labels
+        label_top_left = Gtk.Label(label="This is Top Left")
+        label_top_right = Gtk.Label(label="This is Top Right")
+        label_bottom = Gtk.Label(label="This is Bottom")
+
+        # a grid
         grid = Gtk.Grid()
-        button = Gtk.Button(label="Button")
 
+        # some space between the columns of the grid
+        grid.set_column_spacing(20)
+
+        # in the grid:
+        # attach the first label in the top left corner
+        grid.attach(label_top_left, 0, 0, 1, 1)
+        # attach the second label
+        grid.attach(label_top_right, 1, 0, 1, 1)
+        # attach the third label below the first label
+        grid.attach_next_to(label_bottom, label_top_left, Gtk.PositionType.BOTTOM, 2, 1)
+
+        # add the grid to the window
         self.add(grid)
-        grid.attach(button, 1, 1, 1, 1)
-        self.progress_bar = Gtk.ProgressBar()
-        grid.attach_next_to(self.progress_bar, button, 3, 1, 1)
 
-        button.connect("clicked", self.button_clicked_cb)
-
-    def button_clicked_cb(self, widget):
-        self.progress_bar.pulse()
-
-class GridApplication(Gtk.Application):
+class MyApplication(Gtk.Application):
     def __init__(self):
-        Gtk.Application.__init__(self, application_id="org.gtk.example.grid")
+        Gtk.Application.__init__(self)
 
     def do_activate(self):
-        win = GridWindow(self)
+        win = MyWindow(self)
         win.show_all()
 
-app = GridApplication()
+app = MyApplication()
 exit_status = app.run(sys.argv)
 sys.exit(exit_status)
