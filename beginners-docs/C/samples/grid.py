26c84ad89c17717c7d51c3f2109ea5d35c9a9f77
Marta Maria Casetti <mmcasetti@gmail.com>
2013-03-04 15:31:10 +0000

    GTK+ Python tutorial: page for grid
    The page for grid is now OK - the exercises could be a bit more exciting.

--- beginners-docs/C/samples/grid.py
+++ beginners-docs/C/samples/grid.py
@@ -4,27 +4,19 @@ import sys
 class MyWindow(Gtk.ApplicationWindow):
     def __init__(self, app):
         Gtk.Window.__init__(self, title="Grid Example", application=app)
+        self.set_border_width(5)
 
-        # three labels
         label_top_left = Gtk.Label(label="This is Top Left")
         label_top_right = Gtk.Label(label="This is Top Right")
         label_bottom = Gtk.Label(label="This is Bottom")
 
-        # a grid
         grid = Gtk.Grid()
-
-        # some space between the columns of the grid
         grid.set_column_spacing(20)
 
-        # in the grid:
-        # attach the first label in the top left corner
         grid.attach(label_top_left, 0, 0, 1, 1)
-        # attach the second label
         grid.attach(label_top_right, 1, 0, 1, 1)
-        # attach the third label below the first label
         grid.attach_next_to(label_bottom, label_top_left, Gtk.PositionType.BOTTOM, 2, 1)
 
-        # add the grid to the window
         self.add(grid)
 
 class MyApplication(Gtk.Application):
